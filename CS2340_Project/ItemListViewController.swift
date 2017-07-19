931e88337f6e216f315f8d0bd2a8b7e70cb38bc7
slee3056 <slee3056@gatech.edu>
2017-07-18 20:22:05 -0400

    Add files via upload

---
+++ CS2340_Project/ItemListViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ItemListViewController.swift
+//  CS2340_Project
+//
+//  Created by Seon Gyoo Lee on 7/9/17.
+//  Copyright © 2017 slee3056. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
+//    var array:[String] = []
+//    var array2:[String] = []
+    var filteredData = [String]()
+    var isSearching: Bool = false
+    
+    var beforeViewController:ViewController!
+    
+    //var dict: [String: String] = [:]
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.done
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearching {
+            return filteredData.count
+        }
+        //return dict.count
+        return beforeViewController.arr.count
+        //return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        
+        if isSearching {
+            cell.textLabel?.text = filteredData[indexPath.row]
+        } else {
+//            cell.textLabel?.text = "[" + array2[indexPath.row] + "] " + array[indexPath.row]
+            cell.textLabel?.text = "[" + beforeViewController.arr2[indexPath.row] + "] " + beforeViewController.arr[indexPath.row]
+
+        }
+        
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            isSearching = false
+            view.endEditing(true)
+            tableView.reloadData()
+        } else {
+            isSearching = true
+            filteredData = beforeViewController.arr.filter({$0 == searchBar.text})
+            tableView.reloadData()
+        }
+    }
+
+    @IBAction func onAdd(_ sender: Any) {
+        self.performSegue(withIdentifier: "segItemDetail", sender: -1)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "segItemDetail", sender: indexPath.row)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dest = segue.destination as! ItemDetailViewController
+        dest.beforeViewController = self
+        if (sender as! Int == -1) {
+            //new name
+            dest.isNewName = true
+        } else {
+            let row = sender as! Int
+            let name = beforeViewController.arr[row]
+            let lostfound = beforeViewController.arr2[row]
+            dest.name = name
+            dest.lostOrFound = lostfound
+            dest.isNewName = false
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CS2340_Project/RegisterPageViewController.swift
@@ -0,0 +1,93 @@
+//
+//  RegisterPageViewController.swift
+//  CS2340_Project
+//
+//  Created by Seon Gyoo Lee on 6/22/17.
+//  Copyright © 2017 slee3056. All rights reserved.
+//
+
+import UIKit
+
+class RegisterPageViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    @IBOutlet weak var userID: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onRegister(_ sender: AnyObject) {
+        let username = usernameTextField.text;
+        let password = passwordTextField.text;
+        let repeatPassword = repeatPasswordTextField.text;
+        
+        //check for empty fields
+        if((username?.isEmpty)! || (password?.isEmpty)! || (repeatPassword?.isEmpty)!)
+        {
+            //display alert message
+            displayAlertMessage(userMessage: "All fields are required.");
+            return;
+        }
+        
+        //check if passwords match
+        if(password != repeatPassword)
+        {
+            //display alert message
+            displayAlertMessage(userMessage: "Passwords do not match.")
+            return;
+        }
+        
+        //store data
+        UserDefaults.standard.set(username, forKey:"username");
+        UserDefaults.standard.set(password, forKey:"password");
+        UserDefaults.standard.synchronize();
+        
+        //display alert message with confirmation
+        let myAlert = UIAlertController(title:"Alert", message:"Registration was successful. Thank you!", preferredStyle: UIAlertControllerStyle.alert);
+        
+        let okAction = UIAlertAction(title:"Ok", style:UIAlertActionStyle.default){ action in
+            self.dismiss(animated: true, completion:nil);
+        }
+        
+        myAlert.addAction(okAction);
+        self.navigationController?.popViewController(animated: true)
+        //self.present(myAlert, animated:true, completion:nil);
+    }
+    
+    @IBAction func onCancel(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func displayAlertMessage(userMessage:String)
+    {
+        let myAlert = UIAlertController(title:"Alert", message:userMessage, preferredStyle: UIAlertControllerStyle.alert);
+        
+        let okAction = UIAlertAction(title:"Ok", style:UIAlertActionStyle.default, handler:nil);
+        
+        myAlert.addAction(okAction);
+        
+        self.present(myAlert, animated:true, completion:nil);
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
